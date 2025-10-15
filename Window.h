099626c449cabbd16e8c70b9d5bd8fa87b00477a
Enrique Medrano Solano <enriquemedranorabak@hotmail.com>
2025-10-14 23:19:22 -0600

    Se agregan archivos y carpetas modificadas para la práctica 7, así como los archivos de ejecución .exe y .pdb

---
+++ Window.h
@@ -0,0 +1,46 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	GLfloat getmuevex() { return muevex; }
+	//Para helicoptero
+	GLfloat getmueveXHe() { return mueveXHe; }
+
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);}
+	bool* getsKeys() { return keys; }
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+	GLfloat getarticulacion1() { return articulacion1; }
+	GLfloat getarticulacion8() { return articulacion8; }
+	~Window();
+private: 
+	GLFWwindow *mainWindow;
+	GLint width, height;
+	GLfloat articulacion1, articulacion8;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	void createCallbacks();
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	GLfloat muevex;
+	//para Helicoptero
+	GLfloat mueveXHe;
+	bool mouseFirstMoved;
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+
+};
+
